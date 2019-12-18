597d576104736c41e6323671256abaa29bd86875
syl20ko <sylvain.caron@baliztic.com>
2019-12-18 22:02:37 +0100

    Updates

---
+++ precache-manifest.2feb0cdef679c37f7200b703e2a4c326.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b7e4e89795cab7483e359200329207",
+    "url": "/monstereact/index.html"
+  },
+  {
+    "revision": "fbb06fa8db17386f4c03",
+    "url": "/monstereact/static/css/main.751d7683.chunk.css"
+  },
+  {
+    "revision": "d706f28003964d78c4b2",
+    "url": "/monstereact/static/js/2.1339efe9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monstereact/static/js/2.1339efe9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fbb06fa8db17386f4c03",
+    "url": "/monstereact/static/js/main.5a24eb94.chunk.js"
+  },
+  {
+    "revision": "7269569366e434ad3c71",
+    "url": "/monstereact/static/js/runtime-main.4abb0702.js"
+  }
+]);
